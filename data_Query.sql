30d44151a77fda825dc3df6700e66f93ca2a7115
Karanveer25-code <Karansoodan11@gmail.com>
2025-04-06 14:51:50 -0400

    Update data_Query.sql

--- data_Query.sql
+++ data_Query.sql
@@ -1,7 +1,13 @@
 /*	Question Set 1 - Easy */
 
 /* Q1: Who is the senior most employee based on job title? */
+
 SELECT first_name, last_name, title
 FROM employee
 ORDER BY levels DESC
 LIMIT 1
+
+
+/* Q2: Which countries have the most Invoices? */
+
+
