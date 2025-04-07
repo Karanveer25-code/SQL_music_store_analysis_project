85463e2f0c2ee3670bd5f85cd3cf111a74e3843a
Karanveer25-code <Karansoodan11@gmail.com>
2025-04-07 17:00:25 -0400

    Update data_Query.sql

--- data_Query.sql
+++ data_Query.sql
@@ -44,3 +44,49 @@ LIMIT 1
 
 
 
+/* Question Set 2 - Moderate */
+
+/* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+Return your list ordered alphabetically by email starting with A. */
+
+SELECT c.first_name,c.last_name,c.email
+FROM customer c
+JOIN invoice i ON c.customer_id=i.customer_id
+JOIN invoice_line il ON il.invoice_id=i.invoice_id
+WHERE track_id IN (
+          SELECT track_id From track t
+		  JOIN genre g ON t.genre_id=g.genre_id
+		  WHERE g.name='Rock'
+)
+ORDER BY email
+
+/* Q2: Let's invite the artists who have written the most rock music in our dataset. 
+Write a query that returns the Artist name and total track count of the top 10 rock bands. */
+
+SELECT a.name , COUNT(ab.title) as top_title
+FROM artist a 
+JOIN album ab ON a.artist_id=ab.artist_id
+JOIN track t ON t.album_id=ab.album_id
+JOIN genre g ON g.genre_id=t.genre_id
+WHERE g.name='Rock'
+GROUP BY a.name
+ORDER BY top_title DESC
+LIMIT 10
+
+/* Q3: Return all the track names that have a song length longer than the average song length. 
+Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. */
+
+SELECT name, milliseconds
+FROM track
+WHERE milliseconds > (
+          SELECT AVG(milliseconds)
+		  FROM track
+)
+ORDER BY milliseconds DESC
+
+
+
+
+
+
+
